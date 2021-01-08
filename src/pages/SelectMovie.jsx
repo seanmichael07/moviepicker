0d6262d2ca883678871f18a22cb781a69fa7de09
Sean Burrows <seanmichael07@hotmail.com>
2021-01-09 03:59:58 +1000

    WIP - playing around with progress bar when selecting movie

--- src/pages/SelectMovie.jsx
+++ src/pages/SelectMovie.jsx
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
+import { Button, Progress } from "antd";
 
 class SelectMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: 0,
+    };
+  }
+  tick() {
+    if (this.state.seconds < 100) {
+      this.setState((prevState) => ({
+        seconds: prevState.seconds + 1,
+      }));
+    } else {
+      clearInterval(this.interval);
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 100);
+  }
+
+  componentWillUnmount() {
+    this.interval = setInterval(() => this.tick(), 100);
+  }
+
+  handleRandomSelection = () => {
+    let movies = this.props.movies;
+    let randomMovie = movies[Math.floor(Math.random() * (movies.length - 1))];
+    console.log("random movie =>", randomMovie);
+  };
   render() {
-    return <div>Select movie</div>;
+    // console.log(this.props);
+    return (
+      <div>
+        <Progress type="circle" percent={this.state.seconds} />
+        <Button type="primary" onClick={this.handleRandomSelection}>
+          Select Movie
+        </Button>
+      </div>
+    );
   }
 }
 
