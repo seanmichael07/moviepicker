902b4fd19692ce6203aafb5c5de8193885df7ef9
Sean Burrows <seanmichael07@hotmail.com>
2021-01-09 02:05:36 +1000

    Filled out 404 error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Result, Button } from "antd";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>The page you are looking for does not exist</div>;
+  return (
+    <div className="flex h-full justify-center content-center items-center">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary">
+            <Link to="/dashboard">Back Home</Link>
+          </Button>
+        }
+      />
+    </div>
+  );
 };
 
 export default NotFound;
