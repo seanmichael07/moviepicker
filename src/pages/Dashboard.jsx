743d7ce6060f179ad861c6bb9a5397f8c496bb92
Sean Burrows <seanmichael07@hotmail.com>
2021-01-09 02:05:24 +1000

    Filled out dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Result, Button, Progress } from "antd";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  return <div>Dashboard</div>;
+const Dashboard = (props) => {
+  return (
+    <div className="flex h-full justify-center content-center items-center">
+      <Result
+        title="Dashboard under construction!"
+        extra={
+          <div className="flex flex-col">
+            <Progress
+              type="dashboard"
+              gapDegree={30}
+              percent={68}
+              showInfo={false}
+              style={{ marginBottom: "8%" }}
+            />
+            <div>
+              <Button type="primary">
+                <Link to="/add-movie">Go to Add Movies</Link>
+              </Button>
+            </div>
+          </div>
+        }
+      />
+    </div>
+  );
 };
 
-export default Home;
+export default Dashboard;
