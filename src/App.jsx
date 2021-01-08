a59c5746a6d0c07e30b49a16ede5c797d0b6f8b7
Sean Burrows <seanmichael07@hotmail.com>
2021-01-09 02:44:30 +1000

    Added sorting to collection

--- src/App.jsx
+++ src/App.jsx
@@ -32,5 +32,5 @@ const mapStateToProps = (state) => {
 };
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: "movies" }])
+  firestoreConnect([{ collection: "movies", orderBy: "movieName" }])
 )(App);
