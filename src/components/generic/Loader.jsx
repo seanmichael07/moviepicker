fc0e82cd99502e50352db6cce0e4470abc609918
Sean Burrows <seanmichael07@hotmail.com>
2021-01-08 18:06:20 +1000

    Fixed loader styles

--- src/components/generic/Loader.jsx
+++ src/components/generic/Loader.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import "../../components/generic/Loader"
-import "../../styles/css/loader/loader.css"
+import React from "react";
+import "../../components/generic/Loader";
+import "../../styles/css/loader/loader.css";
 
 const Loader = () => (
-  <div className="h-screen flex justify-center content-center items-center bg-blue-900">
+  <div id="loader-outer-div">
     <svg id="loader">
       <circle cx="70" cy="70" r="70" />
     </svg>
   </div>
-)
+);
 
-export default Loader
+export default Loader;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { createStore, applyMiddleware, compose } from "redux"
-import { Provider, useSelector } from "react-redux"
-import thunk from "redux-thunk"
-import firebase from "firebase/app"
-import { reduxFirestore, getFirestore, createFirestoreInstance } from "redux-firestore"
-import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from "react-redux-firebase"
-import rootReducer from "./store/reducers/rootReducer"
-import firebaseConfig from "./config/firebase.config"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
-import Loader from "./components/generic/Loader"
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider, useSelector } from "react-redux";
+import thunk from "redux-thunk";
+import firebase from "firebase/app";
+import {
+  reduxFirestore,
+  getFirestore,
+  createFirestoreInstance,
+} from "redux-firestore";
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
+} from "react-redux-firebase";
+import rootReducer from "./store/reducers/rootReducer";
+import firebaseConfig from "./config/firebase.config";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Loader from "./components/generic/Loader";
 
-require("dotenv").config()
+require("dotenv").config();
 
 const store = createStore(
   rootReducer,
@@ -20,34 +28,37 @@ const store = createStore(
     applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
     reduxFirestore(firebase, firebaseConfig)
   )
-)
+);
 
 const rrfProps = {
   firebase,
   config: firebaseConfig,
   dispatch: store.dispatch,
   createFirestoreInstance,
-}
+};
 
 function AuthIsLoaded({ children }) {
-  const auth = useSelector(state => state.firebase.auth)
+  const auth = useSelector((state) => state.firebase.auth);
 
-  if (!isLoaded(auth)) {return <Loader />}
+  if (!isLoaded(auth)) {
+    return <Loader />;
+  }
 
-  return children
+  return children;
 }
 
-ReactDOM.render(<Provider store={store}> 
-  <ReactReduxFirebaseProvider {...rrfProps}> 
-    <AuthIsLoaded>
-      <App /> 
-    </AuthIsLoaded>
-  </ReactReduxFirebaseProvider>
-</Provider>, 
-document.getElementById("root")
-)
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
