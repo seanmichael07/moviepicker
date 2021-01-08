db06ce157942baa4b34632ff17faffb3a6f964e4
Sean Burrows <seanmichael07@hotmail.com>
2021-01-09 02:44:06 +1000

    Added notification on success/error when adding a movie

--- src/store/actions/movieActions.js
+++ src/store/actions/movieActions.js
@@ -1,3 +1,4 @@
+import { notification } from "antd"
 export const createMovie = (movie) => {
   return (dispatch, getState, { getFirebase, getFirestore}) => {
     const firestore = getFirestore()
@@ -8,8 +9,24 @@ export const createMovie = (movie) => {
       addedByID: 1234,
       addedAt: new Date()
     }).then(() => {
+      notification.success({
+        message: "Success",
+        description:
+          "You have successfully added " + movie.movieName + " to your list",
+        onClick: () => {
+          console.log("Notification Clicked!");
+        },
+      });
       dispatch({ type: 'CREATE_MOVIE', movie})
     }).catch((err) => {
+      notification.error({
+        message: "Error",
+        description:
+          "There was a problem adding " + movie.movieName + " to your list, please try again later",
+        onClick: () => {
+          console.log("Notification Clicked!");
+        },
+      });
       dispatch({ type: 'CREATE_MOVIE_ERROR', err })
     })
   }
